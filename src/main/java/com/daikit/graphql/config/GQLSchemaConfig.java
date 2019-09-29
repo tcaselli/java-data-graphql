040d911972107ac735b4e5307a445ca65c61f608
Thibaut Caselli <thibaut.caselli@gmail.com>
2019-09-29 20:02:15 +0200

    Provide possibility to retrieve ID value for ID parameter in custom methods

--- src/main/java/com/daikit/graphql/config/GQLSchemaConfig.java
+++ src/main/java/com/daikit/graphql/config/GQLSchemaConfig.java
@@ -34,7 +34,20 @@ public class GQLSchemaConfig {
 	 * Type ID
 	 */
 	public static final class IdType {
-		// Nothing done
+		private String id;
+		/**
+		 * @return the id
+		 */
+		public String getId() {
+			return id;
+		}
+		/**
+		 * @param id
+		 *            the id to set
+		 */
+		public void setId(String id) {
+			this.id = id;
+		}
 	}
 
 	private String queryTypeName = "QueryType";
